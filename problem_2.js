2551d74df486a2822c660c2babb37e58dec2f302
Anas-Qasem <a.qasem@pixilapps.com>
2023-08-22 00:36:20 +0300

    finish problem 2

---
+++ problem_2.js
@@ -0,0 +1,15 @@
+function isPrime(num = 0) {
+   
+    for(let i = 2, s = Math.sqrt(num); i <= s; i++) {
+    console.log(`sqrt =>${Math.sqrt(num)}`)
+        if(num % i === 0) return false;
+    }
+    return num > 1;
+}
+
+console.log(isPrime(0),  false, "0 is not prime");
+console.log(isPrime(1),  false, "1 is not prime");
+console.log(isPrime(2),  true, "2 is prime");
+console.log(isPrime(73), true, "73 is prime");
+console.log(isPrime(75), false, "75 is not prime");
+console.log(isPrime(-1), false, "-1 is not prime");
