cf9175b89d34f993fa2b96abd7be5cde4819c099
anas <a.qasem@pixilapps.com>
2023-09-09 00:43:59 +0300

    enhance problem 8

--- problem_8.js
+++ problem_8.js
@@ -12,10 +12,19 @@ secondMax = (arr =[])=>{
          
         }
     }
+    // that mean array is already sorted or there is only on item
+    if (secondMax == 0) {
+        if (arr.length === 1) {
+            secondMax = max;
+        } else {
+            secondMax = arr[arr.length -2] ;
+        }
+    }
     return secondMax;
     
 }
-console.log( secondMax([1,5, 2])) //== 2
-console.log( secondMax([-1, -5, 2])) //== -1
-console.log( secondMax([-3, -2, -7])) // == -3
-console.log(secondMax([99 , 7 , 6, -33 , 114])) ; // ==99
+// console.log( secondMax([1,5, 2])) //== 2
+console.log( secondMax([-1,-2,-3])) //== 2
+// console.log( secondMax([-1, -5, 2])) //== -1
+// console.log( secondMax([-3, -2, -7])) // == -3
+// console.log(secondMax([99 , 7 , 6, -33 , 114])) ; // ==99
